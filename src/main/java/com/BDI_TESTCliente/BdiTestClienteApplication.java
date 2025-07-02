1d6b6cd90deb0178aeed6011225b0e9e845d0670
Alien 9 <Alien 9@ALIEN9>
2025-07-01 19:02:35 -0600

    Primer commit

---
+++ src/main/java/com/BDI_TESTCliente/BdiTestClienteApplication.java
@@ -0,0 +1,13 @@
+package com.BDI_TESTCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdiTestClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdiTestClienteApplication.class, args);
+	}
+
+}
